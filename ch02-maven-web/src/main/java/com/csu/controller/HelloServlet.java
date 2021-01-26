929179a7c609ccc662946606d95a82c556a54a7f
so1osou1 <1402916604@qq.com>
2021-01-26 10:31:54 +0800

    maven创建web过程

---
+++ ch02-maven-web/src/main/java/com/csu/controller/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.csu.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("Hello Maven Web");
+        pw.flush();
+        pw.close();
+    }
+}
