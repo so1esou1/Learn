495a0aebc340ab7afe2f8086e54c46372a28e805
so1osou1 <1402916604@qq.com>
2021-01-27 17:33:01 +0800

    注解开发mybatis

---
+++ annotate_mybatis/src/main/java/csu/dao/IUserDao.java
@@ -0,0 +1,19 @@
+package csu.dao;
+
+import csu.centralsouth.User;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/*
+用户的持久层接口
+ */
+public interface IUserDao {
+    /**
+     * 查询所有操作
+     * @return
+     */
+    @Select("select * from user")
+    List<User> findAll();
+
+}
