f1ac3f80f37c9acf703d33adabb4d4ba59f851f5
so1osou1 <1402916604@qq.com>
2021-02-03 15:05:35 +0800

    springIOC

---
+++ spring-03-IOC2/src/main/java/com/csu/pojo/User.java
@@ -0,0 +1,25 @@
+package com.csu.pojo;
+
+public class User {
+    private String name;
+
+    public User() {
+        System.out.println("user的无参构造");
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void show(){
+        System.out.println("name" + name);
+    }
+
+}
---
+++ spring-03-IOC2/src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import com.csu.pojo.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyTest {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        User user = (User)context.getBean("asdsad");
+        user.show();
+    }
+}
