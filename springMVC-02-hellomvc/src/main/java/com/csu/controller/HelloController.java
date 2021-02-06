559cddd82f829c370e11dec321a34d8a3252280e
so1osou1 <1402916604@qq.com>
2021-02-06 09:57:44 +0800

    hellospringmvc

---
+++ springMVC-02-hellomvc/src/main/java/com/csu/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.csu.controller;
+
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//先导入Controller接口
+public class HelloController implements Controller {
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        //ModelAndView 模型和视图
+        ModelAndView mv = new ModelAndView();
+
+        //封装对象，放在ModelAndView中，model
+        mv.addObject("msg","HelloSpringMVC!");
+
+        //封装要跳转的视图，放在ModelAndView中
+        mv.setViewName("hello");    // : /WEB/INF/jsp/hello.jsp
+        return mv;
+    }
+
+}
