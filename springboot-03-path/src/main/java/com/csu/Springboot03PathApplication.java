dfeea98b23aeb44d49713ccbb00b8f0d9e1ff6e3
so1osou1 <1402916604@qq.com>
2021-02-09 17:12:18 +0800

    springboot中配置文件的部署位置

---
+++ springboot-03-path/src/main/java/com/csu/Springboot03PathApplication.java
@@ -0,0 +1,13 @@
+package com.csu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03PathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot03PathApplication.class, args);
+    }
+
+}
---
+++ springboot-03-path/src/test/java/com/csu/Springboot03PathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03PathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
