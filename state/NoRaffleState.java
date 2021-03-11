976b47093a6c436e6ac8a4f6ccc78cd059aad884
csustudy <1402916604@qq.com>
2021-03-11 08:37:31 +0800

    状态模式

---
+++ state/NoRaffleState.java
@@ -0,0 +1,35 @@
+package com.csu.state;
+/*
+    不可以抽奖的状态类,初始化时处于这个状态
+ */
+public class NoRaffleState extends State{
+
+    // 初始化时传入活动引用，扣除积分后改变其状态
+    RaffleActivity activity;
+
+    public NoRaffleState(RaffleActivity activity) {
+        this.activity = activity;
+    }
+
+    // 当前状态可以扣积分 , 扣除后，将状态设置成可以抽奖状态
+    @Override
+    public void deductMoney() {
+        System.out.println("扣除50积分成功，您可以抽奖了");
+        activity.setState(activity.getCanRaffleState());
+    }
+
+
+    // 当前状态不能抽奖
+    @Override
+    public boolean raffle() {
+        System.out.println("扣了积分才能抽奖喔！");
+        return false;
+    }
+
+
+    // 当前状态不能发奖品
+    @Override
+    public void dispensePrize() {
+        System.out.println("不能发放奖品");
+    }
+}
